7acef8dc633a952ae10e3496c94aa8c5fac4d7ee
Tim Riep <tim@riep-tech.de>
2026-03-28 12:25:22 +0100

    Added Swagger and Swagger UI

--- packages/backend/src/app/makeApp.ts
+++ packages/backend/src/app/makeApp.ts
@@ -1,6 +1,8 @@
 import fastifyEnv from "@fastify/env";
 import { fastify, type FastifyInstance } from "fastify";
 import configSchema from "./configSchema.js";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 export default async () : Promise<FastifyInstance> => {
     const app = fastify({
@@ -10,10 +12,20 @@ export default async () : Promise<FastifyInstance> => {
     await app.register(fastifyEnv, {
         confKey:'config',
         schema:configSchema,
-        dotenv:{
-            debug:true
+        dotenv:true
+    })
+
+    await app.register(fastifySwagger, {
+        openapi: {
+            openapi:"3.0.0",
+            info:{
+                title:'XSL Tool API',
+                version:'0.0.1'
+            }
         }
     })
+
+    await app.register(fastifySwaggerUi)
 
     return app;
 }
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,6 +2,8 @@ import makeApp from "./app/makeApp.js";
 
 const app = await makeApp()
 
+await app.ready()
+
 void app.listen({
     port:app.config.PORT
 })
